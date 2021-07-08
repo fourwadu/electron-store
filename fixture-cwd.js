739148713a026a6ab1d06e9e9a3655de0f22014a
noah <noah.sigel@gmail.com>
2021-07-08 10:15:36 -0400

    IPC Change

--- fixture-cwd.js
+++ fixture-cwd.js
@@ -1,22 +1,22 @@
-'use strict';
-const path = require('path');
-const electron = require('electron');
-const Store = require('.');
+"use strict";
+const path = require("path");
+const electron = window.require("electron");
+const Store = require(".");
 
 // Prevent Electron from never exiting when an exception happens
-process.on('uncaughtException', error => {
-	console.error('Exception:', error);
+process.on("uncaughtException", (error) => {
+	console.error("Exception:", error);
 	process.exit(1);
 });
 
-console.log(electron.app.getPath('userData'));
+console.log(electron.app.getPath("userData"));
 
-const store = new Store({cwd: 'foo'});
-store.set('unicorn', '🦄');
+const store = new Store({ cwd: "foo" });
+store.set("unicorn", "🦄");
 console.log(store.path);
 
-const store2 = new Store({cwd: path.join(__dirname, 'bar')});
-store2.set('ava', '🚀');
+const store2 = new Store({ cwd: path.join(__dirname, "bar") });
+store2.set("ava", "🚀");
 console.log(store2.path);
 
 electron.app.quit();
